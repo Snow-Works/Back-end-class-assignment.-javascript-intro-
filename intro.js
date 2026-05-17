5421dab6dacd64f0b4c7853f458b51c6f6ecbb6a
Snow-Works <snownation274@gmail.com>
2026-05-17 16:19:44 +0100

    Add files via upload
    BeTechiefied assignment

---
+++ intro.js
@@ -0,0 +1,9 @@
+// intro.js
+
+const name = "Michael Samuel";
+const age = 22; 
+const favoriteLanguage = "Python programming language";
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Favorite Programming Language: ${favoriteLanguage}`);
